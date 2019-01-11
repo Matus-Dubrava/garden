8cfebd7928256ca18b88fd157d478db8209b8b10
matus dubrava <matusdubrava@gmail.com>
2019-01-11 11:36:35 +0100

    added build files

---
+++ client/build/precache-manifest.c22e34fbd79b3e21bd8b3add15b5cbb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1237ba00bb018bc650b",
+    "url": "/static/js/main.e1237ba0.chunk.js"
+  },
+  {
+    "revision": "b6c2f703ee516c007b2f",
+    "url": "/static/js/1.b6c2f703.chunk.js"
+  },
+  {
+    "revision": "e1237ba00bb018bc650b",
+    "url": "/static/css/main.1757e264.chunk.css"
+  },
+  {
+    "revision": "4db138efb2318b10d7bfa6a2788aef55",
+    "url": "/index.html"
+  }
+];
